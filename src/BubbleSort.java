1ffa76503cbe302c0e2e16588d194b195e05aeef
Your Name <dat054760@gmail.com>
2022-08-10 11:03:16 +0700

    up

---
+++ src/BubbleSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static void BubbleSort(int[] list) {
+        boolean needNextPass = true;
+
+        for (int i = 1; i < list.length && needNextPass; i++) {
+            needNextPass = false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BubbleSort(list);
+        System.out.println(Arrays.toString(list));
+    }
+}
